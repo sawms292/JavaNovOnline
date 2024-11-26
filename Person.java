0818a4a48d5f607cdba27c696ee9a22c9ffed154
Saw Ming Sheng <sa_ming_sheng@soc.uum.edu.my>
2024-11-26 16:40:27 +0800

    Person

---
+++ Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    public static void main(String[] args) {
+        String name ="Saw";
+        int age = 24;
+        double height = 1.77;
+        boolean isUumStudent = true;
+        long netWorth = 1000000000L;
+        System.out.println("The student name is : " + name + " and age is " + age);
+        System.out.println("The student height is : " + height + " and is a UUM student" + isUumStudent);
+        System.out.println("The student net worth is : " + netWorth);
+    }
+
+}
---
+++ VariableScope.java
@@ -0,0 +1,4 @@
+public class VariableScope {
+    public static void main(String[] args) {
+    }
+}
