4de6000dfa681ec5fa7ec9d7eb669aba922d468c
Saw Ming Sheng <sa_ming_sheng@soc.uum.edu.my>
2024-11-26 17:03:40 +0800

    Person

--- VariableScope.java
+++ VariableScope.java
@@ -1,4 +1,32 @@
 public class VariableScope {
+
+    // Instance variables
+    int age = 24;            
+    String name = "saw"; 
+    double height = 1.79;    
+
+    // Static variable
+    static int staticVariable = 26;
+
+    public void showLocalVariable() {
+        int localVariable = 30;
+        System.out.println("Local Variable: " + localVariable);
+        System.out.println("Instance Variable (age): " + age);
+        System.out.println("Instance Variable (name): " + name);
+        System.out.println("Instance Variable (height): " + height);
+        System.out.println("Static Variable: " + staticVariable);
+    }
+
+    public void showInstanceVariable() {
+        System.out.println("Instance Variable (age): " + age);
+        System.out.println("Instance Variable (name): " + name);
+        System.out.println("Instance Variable (height): " + height);
+        System.out.println("Static Variable: " + staticVariable);
+    }
+
     public static void main(String[] args) {
+        VariableScope newvar = new VariableScope();
+        newvar.showLocalVariable();
+        newvar.showInstanceVariable();
     }
 }
